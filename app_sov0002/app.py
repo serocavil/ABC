f3834e429af1b55d7225ed299cc783ac32a10a6e
serocavil <sov0002@alu.medac.es>
2025-11-28 14:03:24 +0100

    Update app.py

--- app_sov0002/app.py
+++ app_sov0002/app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Obtener el secreto desde la variable de entorno
+        secreto = os.getenv("SECRETO", "No hay secreto configurado")
 
-@app.route("/")
-def home():
-    return "hola SOV0002"
+        contenido = f"""
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Hola SOV0002</title>
+        </head>
+        <body style="background:#f6f6f6; display:flex; justify-content:center; align-items:center; height:100vh; margin:0;">
+            <h1 style="font-family:Arial; color:#000;">
+                hola SOV0002, mi secreto es: {secreto}
+            </h1>
+        </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(contenido.encode("utf-8"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081)
+    # Cambiamos aquí a 8001
+    servidor = HTTPServer(("0.0.0.0", 8001), SimpleHandler)
+    print("Servidor iniciado en http://localhost:8001")
+    servidor.serve_forever()
